d5227491c514962769d6a9977247e72c5a5ca6ab
raza-jafri <arjafri@gmail.com>
2014-11-02 21:58:11 -0600

    Part 1 complete
    working on part II

--- app/models/zodiac.rb
+++ app/models/zodiac.rb
@@ -1,5 +1,5 @@
 class Zodiac
-  attr_accessor :creature, :sign, :fortune
+attr_accessor :creature, :sign, :fortune
 
   def Zodiac.all
     info = [
@@ -64,8 +64,7 @@ class Zodiac
         :fortune => "Business and financial success makes you happy and satisfied, Pisces. You also look forward to moving on. The downside of this flush period is that people who aren't particularly trustworthy might decide to latch onto your coattails for their own purposes. Some might even ask for loans. Be careful about the ones you assist. They might not be honest with you. Don't fall for any sob stories."
        }
     ]
-
-    return info.map do |hash|
+      return info.map do |hash|
       z = Zodiac.new
       z.creature = hash[:creature]
       z.sign = hash[:sign]
